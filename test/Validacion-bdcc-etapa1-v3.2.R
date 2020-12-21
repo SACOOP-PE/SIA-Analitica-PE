e75e9896754b48e1d4d5dfa4a63e59c365532217
Edwin Roque Rojas <49539600+edwin123q@users.noreply.github.com>
2020-12-21 09:21:37 -0500

    Update Validacion-bdcc-etapa1-v3.2.R

--- test/Validacion-bdcc-etapa1-v3.2.R
+++ test/Validacion-bdcc-etapa1-v3.2.R
@@ -14,7 +14,7 @@ listaErrores %>%
   mutate(n_caracteres = nchar(Detalle)) %>% 
   select(Cod, Descripcion, n_caracteres)
 
-#### encontrar archivos en cada errror de la listaErrores
+#### encontrar archivos y periodos en cada errror de la listaErrores
 listaErrores %>%
   mutate(Detalle = map_chr(Detalle, ~ .[[1]] %>% str_c(collapse = ", "))) %>%
   rowwise() %>%
@@ -26,58 +26,40 @@ listaErrores %>%
                                               str_split(",")),
                                      paste(alcanceGeneral, collapse = '|'))[is.na(str_extract(unlist(Detalle %>% str_split(",")),
                                                                                               paste(alcanceGeneral,collapse = '|'))) == FALSE] %>%
-           unique() %>% toString()
-         ) %>%
+           unique() %>% toString()) %>%
   select(Cod, Descripcion, ArchivosError, PeriodosError) %>% view()
 
-# resumen_errores_periodos
-listaErrores %>%
-  mutate(Detalle = map_chr(Detalle, ~ .[[1]] %>% str_c(collapse = ", "))) %>%
-  rowwise() %>%
-  mutate(PeriodosError = str_extract(unlist(Detalle %>%
-                                               str_split(",")),
-                                     paste(alcanceGeneral, collapse = '|'))[is.na(str_extract(unlist(Detalle %>% str_split(",")),
-                                                                                              paste(alcanceGeneral,collapse = '|'))) == FALSE] %>%
-    unique() %>% toString()) %>%
-  select(Cod, Descripcion, PeriodosError) %>%
-  view()
-
 #####
-#periodos en un error de terminado:
+#guardar algunas observaciones en cvs:
 saveObservacion <- function(codError){
- tb <- tibble(creditos_split = listaErrores %>% filter(Cod == codError) %>% pull(Detalle) %>% 
+  tb <- tibble(creditos_split = listaErrores %>% filter(Cod == codError) %>% pull(Detalle) %>% 
                                   strsplit(split = ")") %>% unlist(),
-              PeriodosError  = creditos_split %>% 
-                                  str_extract(paste(alcanceGeneral, collapse = '|')) %>% 
+              PeriodosError  = str_extract(creditos_split, paste(alcanceGeneral, collapse = '|')) %>% 
                 unique()) %>%
-   rowwise() %>% 
-   mutate(creditosPeriodo = str_extract(creditos_split %>% str_split(pattern = ",") %>% unlist(),
-                                        paste(getInfoTotal(getCarpeta(header), PeriodosError, "BD01") %>% pull(CCR),
-                                              collapse = '|'))[is.na(str_extract(creditos_split %>% str_split(pattern = ",") %>% unlist(),
-                                                                                 paste(getInfoTotal(getCarpeta(header), PeriodosError, "BD01") %>% pull(CCR),
-                                                                                       collapse = '|')))==FALSE] %>%
-            unique %>% toString()) %>% 
+    rowwise() %>% 
+    mutate(creditosPeriodo = str_extract(unlist(creditos_split %>% str_split(pattern = ",")),
+                                         paste(getInfoTotal(getCarpeta(header), PeriodosError, "BD01") %>% pull(CCR),
+                                               collapse = '|'))[is.na(str_extract(unlist(creditos_split %>% str_split(pattern = ",")),
+                                                                                  paste(getInfoTotal(getCarpeta(header), PeriodosError, "BD01") %>% pull(CCR),
+                                                                                        collapse = '|')))==FALSE] %>%
+             unique %>% toString()) %>% 
     select(PeriodosError, creditosPeriodo)
   
   periodos <- tb %>% pull(PeriodosError)
   creditos <- tb %>% 
-                filter(PeriodosError == periodos[1]) %>%
-                pull(creditosPeriodo) %>%
+                filter(PeriodosError == periodos[1]) %>% pull(creditosPeriodo) %>%
                 str_split(pattern = ",") %>% unlist() %>% 
                 str_replace_all(pattern=" ", repl="") %>% return()
+  observacionBD <- getInfoTotal(getCarpeta(header), periodos[1], "BD01") %>%
+                        filter(CCR %in% creditos) %>% mutate(Periodo = periodos[1])
   
   if(length(periodos) == 1){
-    observacionBD <- getInfoTotal(getCarpeta(header), periodos, "BD01") %>%
-      filter(CCR %in% creditos) %>% mutate(Periodo = periodos)
-    
     observacionBD <- observacionBD %>%
       select(Periodo, unlist(getColumnasOM("BD01")))
       return(observacionBD)
   }
   if(length(periodos) > 1){
-  observacionBD <- getInfoTotal(getCarpeta(header), periodos[1], "BD01") %>%
-                        filter(CCR %in% creditos) %>% mutate(Periodo = periodos[1])
-  for (i in 1:(length(periodos)-1)){
+    for (i in 1:(length(periodos)-1)){
       creditos_i <- tb %>% filter(PeriodosError == periodos[i+1]) %>% pull(creditosPeriodo) %>%
                             str_split(pattern = ",") %>% unlist() %>%
                             str_replace_all(pattern=" ", repl="")
@@ -87,12 +69,11 @@ saveObservacion <- function(codError){
 
       observacionBD <- bind_rows(observacionBD, observacionBD_i)
       }
-  observacionBD <- observacionBD %>%
+    observacionBD <- observacionBD %>%
     select(Periodo, unlist(getColumnasOM("BD01")))
     return(observacionBD)
   }
 }
-
 saveObservaciones <- function(){
   codErroresActuales <- listaErrores %>% pull(Cod) %>% 
     setdiff(c(201, 202, 203, 301, 302, 303, 304, 431, 432, 441, 461, 466, 467))
