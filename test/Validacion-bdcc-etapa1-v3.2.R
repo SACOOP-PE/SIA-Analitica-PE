4fed51cfedb5be55feeff35e193db1478b931c85
Edwin Roque Rojas <49539600+edwin123q@users.noreply.github.com>
2020-12-21 18:31:01 -0500

    Update Validacion-bdcc-etapa1-v3.2.R

--- test/Validacion-bdcc-etapa1-v3.2.R
+++ test/Validacion-bdcc-etapa1-v3.2.R
@@ -1,9 +1,9 @@
 ##### Testing -----
-header      <- initHeader(idCoopac = "01172",
-                          coopacCarpeta  = "test/datatest/",
-                          periodoInicial = "201901",
-                          periodoFinal   = "202010",
-                          bds            = list(c("BD01", "BD02A", "BD02B", "BD03A", "BD03B", "BD04")))
+header       <- initHeader(idCoopac = "01172",
+                           coopacCarpeta  = "test/datatest/",
+                           periodoInicial = "201901",
+                           periodoFinal   = "202010",
+                           bds            = list(c("BD01", "BD02A", "BD02B", "BD03A", "BD03B", "BD04")))
 eb           <- initBucketErrores(header)
 listaErrores <- main(header, eb)
 
