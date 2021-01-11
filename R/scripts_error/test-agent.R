ebce0eedf8db45e9fa2e98bcd92c88da2afc817a
Edwin Roque Rojas <49539600+edwin123q@users.noreply.github.com>
2021-01-11 14:15:16 -0500

    Update test-agent.R

--- R/scripts_error/test-agent.R
+++ R/scripts_error/test-agent.R
@@ -1,20 +1,38 @@
 rm(list=ls())
-source("R/scripts_error/libraries-and-options.R")
 source("R/scripts_error/agent.R")
-source("R/scripts_error/utils.R")
 source("R/scripts_error/error-handler.R")
-source("R/scripts_error/reporting-manager.R")
 source("R/scripts_error/layer0-prerequisites.R")
 source("R/scripts_error/layer1-validation-error1.R")
+source("R/scripts_error/layer2-validation-error2-alpha.R")
 source("R/scripts_error/layer2-validation-error2.R")
+source("R/scripts_error/libraries-and-options.R")
+source("R/scripts_error/log-manager.R")
+source("R/scripts_error/reporting-manager.R")
+source("R/scripts_error/utils.R")
+
+# Cerrar agente.
+# Formatear bucket generado para comunicarlo por correo o por Oficio SBS. 
+# getArchivosFromAgent(agent)
+# getArchivosNoObservados(agent,bucket)
+# getArchivosNoObservadosByCols(agent, bucket, c("MON_C", "MCUO_C", "X29"))
+# getArchivosNoObservadosByErrors(agent,bucket,c(201))
+# getArchivosObservadosFromBucket(bucket)
+# Inicializar agente.
+# Interrogar agente - Layer del 0 al 3. 
+# Nuevas funciones para error-handling
+# print(ebFormatted)
 ##### Testing -----
+agent <- createAgent(idCoopac = "01172",
+                     usuarioSIA = "DPACHECO",
+                     coopacCarpeta  = "test/datatest",
+                     periodoFinal   = "202006",
+                     periodoInicial = "201907",
+                     bds            = list(c("BD01", "BD02A", "BD02B", "BD03A", "BD03B", "BD04")))
 
-# Inicializar
-agent <- create_agent(idCoopac = "01172",
-                      coopacCarpeta  = "test/datatest/",
-                      periodoInicial = "201901",
-                      periodoFinal   = "202010",
-                      bds            = list(c("BD01", "BD02A", "BD02B", "BD03A", "BD03B", "BD04")))
-
-bucket  <- interrogate(agent)
-bucket2 <- procesarBucket2(agent, bucket)
+agent       <- closeAgent(agent, bucket)
+bucket      <- interrogateAgent(agent)
+ebFormatted <- formatBucket(bucket)
+print("**")
+print(agent)
+print(bucket)
+print(ebFormatted)
