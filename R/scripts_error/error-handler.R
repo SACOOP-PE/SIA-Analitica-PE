cd49cb46c87521b0e562d267cd631caf59ca3ebb
Edwin Roque Rojas <49539600+edwin123q@users.noreply.github.com>
2021-01-18 16:50:18 -0500

    Change AddError for addErrorIndividual #16

--- R/scripts_error/error-handler.R
+++ R/scripts_error/error-handler.R
@@ -1,35 +1,10 @@
 ## Gestion de errores----
-addErrorIndividual <- function(eb, agente, codcoopac, idproceso, cod, periodo, bd, 
-                               arg_txt1, arg_txt2, arg_txt3,
-                               arg_num1, arg_num2, arg_num3){
-  t <- tibble(CodCoopac = codcoopac,
-              IdProceso  = idproceso, 
-              Cod         = cod, 
-              Periodo     = periodo,
-              BD          = bd,
-              txt1     = arg_txt1,
-              txt2    = arg_txt2,
-              txt3    =  arg_txt3,
-              num1    = arg_num1,
-              num2    = arg_num2,
-              num3    = arg_num3)
- 
-
-  return(bind_rows(eb, t))
-} 
+addError <- function(eb, obj) {
+  
+  eb <- bind_rows(eb, obj)
+  return(eb)
+}
 
 addErrorMasivo <- function(eb, parte1){
   bind_rows(eb, parte1) %>% return()
 }
-
-
-
-# Edwin reducir las dos funciones a esto: 
-
-# addError <- function(eb, 
-#                      obj = NULL) {
-#   
-#   eb <- bind_rows(eb, obj)
-#   return(eb)
-#   
-# }
--- R/scripts_error/layer0-validation-error0.R
+++ R/scripts_error/layer0-validation-error0.R
@@ -7,39 +7,30 @@ layer0 <- function(agente, eb){
   
   eb <- eb %>% filter(Cod != 100) 
   
-  if (length(getDuplicados(carpeta, exigibles)) != 0) { 
+  if (length(getDuplicados(carpeta, exigibles)) != 0) {
     
-    eb <- eb %>% addErrorIndividual(agente, codcoopac = getCoopacFromAgent(agente),
-                                    idproceso = getIdProcesoFromAgent(agente),
-                                    cod = 101,
-                                    periodo = "",
-                                    bd = "",
-                                    arg_txt1 = toString(getDuplicados(carpeta, exigibles)),
-                                    arg_txt2 ="",
-                                    arg_txt3 = "",
-                                    arg_num1 = length(getDuplicados(carpeta, exigibles)),
-                                    arg_num2 = 0,
-                                    arg_num3 = 0)
+    eb <- eb %>% addError(obj = tibble(codcoopac = getCoopacFromAgent(agente),
+                                       idproceso = getIdProcesoFromAgent(agente),
+                                       cod = 101,
+                                       txt1 = toString(getDuplicados(carpeta, exigibles)),
+                                       num1 = length(str_split(txt1, ",")[[1]]))
+                          ) 
+    
     addEventLog(agent, paste0("    Error: Se identificaron archivos duplicados."), "I", "B")
   }
  
   if (length(getFaltantes(carpeta, exigibles)) != 0) { 
     
-    eb <- eb %>% addErrorIndividual(agente, codcoopac = getCoopacFromAgent(agente),
-                                    idproceso = getIdProcesoFromAgent(agente),
-                                    cod = 102,
-                                    periodo = "",
-                                    bd = "",
-                                    arg_txt1 = toString(getFaltantes(carpeta, exigibles)),
-                                    arg_txt2 ="",
-                                    arg_txt3 = "",
-                                    arg_num1 = length(getFaltantes(carpeta, exigibles)),
-                                    arg_num2 = 0,
-                                    arg_num3 = 0)
+    eb <- eb %>% addError(obj = tibble(codcoopac = getCoopacFromAgent(agente),
+                                       idproceso = getIdProcesoFromAgent(agente),
+                                       cod = 102,
+                                       txt1 = toString(getFaltantes(carpeta, exigibles)),
+                                       num1 = length(str_split(txt1, ",")[[1]]))
+                          )
+
     addEventLog(agent, paste0("    Error: Se identificaron archivos faltantes."), "I", "B")
   }
  
-  
   return(eb)
 }
 
