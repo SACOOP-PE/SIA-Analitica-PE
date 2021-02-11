b0a4a3e9aa70f511f1a9cd9b731f198f8a241e49
Edwin Roque Rojas <49539600+edwin123q@users.noreply.github.com>
2021-02-11 09:59:22 -0500

    Update reporting-manager.R

--- R/scripts-entorno/reporting-manager.R
+++ R/scripts-entorno/reporting-manager.R
@@ -5,57 +5,57 @@ formatBucket <- function(eb) {
                           pad2(num1), 
                           format(num1, big.mark = ",", small.interval = 3)),
            num2 = format(num2, big.mark = ",", small.interval = 3),
-           Descripcion = if_else(Cod <= 102,
-                                 str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                      "\\Q{1}"  = txt1)),
-                                 if_else(Cod >=201 & Cod <= 203,
-                                         str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                              "\\Q{1}"  = txt1,
-                                                                              "\\Q{2}"  = BD,
-                                                                              "\\Q{3}"  = periodoEscrito(Periodo))),
-                                         if_else(Cod >= 301 & Cod <= 308, 
-                                                 str_replace_all(getDescError(Cod), c("\\Q{0}"  = tolower(toString(months(as.Date(paste(substr(Periodo,1,4), substr(Periodo,5,6), "01", sep = "-"))))),
-                                                                                      "\\Q{1}"  = periodoEscrito(Periodo),
-                                                                                      "\\Q{2}"  = toString(num2))),
-                                                 if_else(Cod == 401 | Cod == 402,
-                                                         str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                              "\\Q{1}"  = tolower(toString(months(as.Date(paste(substr(Periodo,1,4), substr(Periodo,5,6), "01", sep = "-"))))),
-                                                                                              "\\Q{2}"  = toString(num2),
-                                                                                              "\\Q{3}"  = periodoEscrito(Periodo))),
-                                                         if_else(Cod == 403 | Cod == 501 | Cod == 503,
-                                                                 str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                      "\\Q{1}"  = periodoEscrito(Periodo))),
-                                                                 if_else(Cod == 404 | Cod == 502,
-                                                                         str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                              "\\Q{1}"  = toString(num2),
-                                                                                                              "\\Q{2}"  = periodoEscrito(Periodo))),
-                                                                         if_else(Cod == 405,
-                                                                                 str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                                      "\\Q{1}"  = txt2)),
-                                                                                 if_else(Cod >= 601 & Cod <= 621,
-                                                                                         str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                                              "\\Q{1}"  = periodoEscrito(Periodo),
-                                                                                                                              "\\Q{2}"  = txt3)),
-                                                                                         if_else(Cod == 622,
-                                                                                                 str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                                                      "\\Q{1}"  = txt2,
-                                                                                                                                      "\\Q{2}"  = BD,
-                                                                                                                                      "\\Q{3}"  = periodoEscrito(Periodo))),
+           Descripcion = replaceString(if_else(Cod <= 102,
+                                               str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                    "\\Q{1}"  = txt1)),
+                                               if_else(Cod %in% c(201:203),
+                                                       str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                            "\\Q{1}"  = txt1,
+                                                                                            "\\Q{2}"  = BD,
+                                                                                            "\\Q{3}"  = periodoEscrito(Periodo))),
+                                                       if_else(Cod %in% c(301:308), 
+                                                               str_replace_all(getDescError(Cod), c("\\Q{0}"  = tolower(toString(months(as.Date(paste(substr(Periodo,1,4), substr(Periodo,5,6), "01", sep = "-"))))),
+                                                                                                    "\\Q{1}"  = periodoEscrito(Periodo),
+                                                                                                    "\\Q{2}"  = toString(num2))),
+                                                               if_else(Cod == 401 | Cod == 402,
+                                                                       str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                            "\\Q{1}"  = tolower(toString(months(as.Date(paste(substr(Periodo,1,4), substr(Periodo,5,6), "01", sep = "-"))))),
+                                                                                                            "\\Q{2}"  = toString(num2),
+                                                                                                            "\\Q{3}"  = periodoEscrito(Periodo))),
+                                                                       if_else(Cod == 403 | Cod == 501 | Cod == 503,
+                                                                               str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                    "\\Q{1}"  = periodoEscrito(Periodo))),
+                                                                               if_else(Cod == 404 | Cod == 502,
+                                                                                       str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                            "\\Q{1}"  = toString(num2),
+                                                                                                                            "\\Q{2}"  = periodoEscrito(Periodo))),
+                                                                                       if_else(Cod == 405,
+                                                                                               str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                                    "\\Q{1}"  = txt2)),
+                                                                                               if_else(Cod >= 601 & Cod <= 621,
+                                                                                                       str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                                            "\\Q{1}"  = periodoEscrito(Periodo),
+                                                                                                                                            "\\Q{2}"  = txt3)),
+                                                                                                       if_else(Cod == 622,
+                                                                                                               str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                                                    "\\Q{1}"  = txt2,
+                                                                                                                                                    "\\Q{2}"  = BD,
+                                                                                                                                                    "\\Q{3}"  = periodoEscrito(Periodo))),
                                                                                                  
-                                                                                                 if_else(Cod >= 701,
-                                                                                                         str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
-                                                                                                                                              "\\Q{1}"  = periodoEscrito(Periodo))),
-                                                                                                         ""
-                                                                                                         )
-                                                                                                 )
-                                                                                         )
-                                                                                 )
-                                                                         )
-                                                                 )
-                                                         )
-                                                 )
-                                         )
-                                 ),
+                                                                                                               if_else(Cod >= 701,
+                                                                                                                       str_replace_all(getDescError(Cod), c("\\Q{0}"  = toString(num1),
+                                                                                                                                                            "\\Q{1}"  = periodoEscrito(Periodo))),
+                                                                                                                       ""
+                                                                                                                       )
+                                                                                                              )
+                                                                                                       )
+                                                                                              )
+                                                                                      )
+                                                                               )
+                                                                       )
+                                                               )
+                                                       )
+                                               ), num1, Cod),
            Categoria  = getCateError(Cod),
            Criticidad = getCritError(Cod),
            Tipo       = "Error"
@@ -89,7 +89,12 @@ cutStringError <- function(num1, txt1) {
   }
   else{return(txt1)}
 }
-
+replaceString  <- function(desc, num1, cod) {
+  desc <- if_else(num1 == "01" & (cod %in% c(301:308) == FALSE), 
+                  str_replace(desc, "Se detectaron", "Se detectó"),
+                  desc)
+  return(desc)
+}
 
 #Descr periodos
 pad2            <- function(n) {
